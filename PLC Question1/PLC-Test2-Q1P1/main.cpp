2985198a99affd04f71cae44e6bc9cc5dd2e0108
evgeniyko99 <59245649+evgeniyko99@users.noreply.github.com>
2020-04-24 23:21:33 -0400

    Add files via upload

--- PLC Question1/PLC-Test2-Q1P1/main.cpp
+++ PLC Question1/PLC-Test2-Q1P1/main.cpp
@@ -1,43 +1,27 @@
 #include <iostream>
 
-enum numbers{
-	one = 1,
-	two = 2,
-	three = 3,
-	four = 4,
-	five = 5,
-	six = 6,
-	seven = 7,
-	eight = 8,
-	nine = 9,
-	ten = 10,
+enum daysOfTheWeek {
+  monday = 1, 
+  tuesday = 2,
+  wednesday = 3,
+  thursday = 4,
+  friday = 5,
+  saturday = 6,
+  sunday = 7
 };
-	
-enum random{
-	hundred = 100,
-  twohundred = 200,
+
+enum classes {
+  noClasses = 0,
+  classes = 1
 };
 
 int main() {
-  std::cout << (one+ten) << std::endl;
-  std::cout << (two+nine) << std::endl;
-  std::cout << (three+eight) << std::endl;
-  std::cout << (four+seven) << std::endl;
-  std::cout << (five+six) << std::endl;
-  std::cout << (ten-one) << std::endl;
-  std::cout << (nine-two) << std::endl;
-  std::cout << (eight-three) << std::endl;
-  std::cout << (seven-four) << std::endl;
-  std::cout << (six-five) << std::endl;
-  std::cout << (hundred*twohundred) << std::endl;
-  std::cout << (hundred+one) << std::endl;
-  std::cout << (hundred/two) << std::endl;
-  std::cout << (hundred*three) << std::endl;
-  std::cout << (hundred/four) << std::endl;
-  std::cout << (hundred/five) << std::endl;
-  std::cout << (hundred*six) << std::endl;
-  std::cout << (hundred*seven) << std::endl;
-  std::cout << (hundred*eight) << std::endl;
-  std::cout << (hundred*nine) << std::endl;
-  std::cout << (hundred/ten) << std::endl;
-};
+
+  int today = 1;
+
+  if(today > 2 && today < 5) {
+    std::cout << "You have classes today" << std::endl;
+  } else {
+    std::cout << "You do not have classes today" << std::endl;
+  }
+}
